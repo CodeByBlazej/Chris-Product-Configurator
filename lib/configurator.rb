f298fa7e7e8d1e24f5cae19b7bbe5eb7b95dc9bf
CodeByBlazej <codebyblazej@gmail.com>
2025-05-21 22:04:01 +0100

    add methods to create all variables

--- lib/configurator.rb
+++ lib/configurator.rb
@@ -1,37 +1,57 @@
+require_relative 'configurator/gold_range'
+require_relative 'configurator/bronze_range'
+require_relative 'configurator/silver_range'
+require_relative 'configurator/double_infill_dividers'
+
 class Configurator
-  attr_reader :front_range_selected
+  attr_reader :product_range, :product_size, :product_material, :current_product, :product_amount
   
   def initialize
-    @front_range_selected = nil
-    @front_size = nil
-    @divider_size = nil
-    @front_material = nil
-    @divider_material = nil
+    @product_range = nil
+    @product_size = nil
+    @product_material = nil
     @current_product = nil
+    @product_amount = nil
+    @cart = {}
   end
 
   def configure_product
-    select_fronts
+    select_product
+    select_range
     select_size
     select_material
+    select_amount
+    add_product_to_cart
   end
 
-  def select_fronts
-    puts 'Select the range of fronts by typing: gold, silver or bronze'
-    front_range = gets.chomp.downcase
+  def select_product
+    puts 'What product do you want to customise? Type FRONT or DIVIDER'
+    product = gets.chomp.downcase
 
-    until front_range == 'gold' || front_range =='silver' || front_range == 'bronze' do
+    until product == 'front' || product == 'divider'
       puts "Oops! Looks like typo, please type again..."
-      front_range = gets.chomp.downcase
+      product = gets.chomp.downcase
     end
 
-    @front_range_selected = front_range
-    @current_product = 'front'
-    puts "Good choice! You selected #{front_range_selected.upcase} front"
+    @current_product = product
+    puts "You selected #{current_product.upcase} product"
+  end
+
+  def select_range
+    puts 'What product range would you prefer? Type GOLD, SILVER or BRONZE'
+    range = gets.chomp.downcase
+
+    until range == 'gold' || range =='silver' || range == 'bronze' do
+      puts "Oops! Looks like typo, please type again..."
+      range = gets.chomp.downcase
+    end
+
+    @product_range = range
+    puts "You selected #{product_range.upcase} front"
   end
 
   def select_size
-    puts 'What size of them do you need?/n Available sizes are: 8x7ft | 10x7ft | 12x7ft | 14x7ft/n Type only 1st number'
+    puts "What size of your #{current_product.upcase} do you need?\nAvailable sizes are: 8x7ft | 10x7ft | 12x7ft | 14x7ft\nType only 1st number"
     size = gets.chomp.downcase
 
     until size == '8' || size == '10' || size == '12' || size == '14' do
@@ -39,12 +59,12 @@ class Configurator
       size = gets.chomp.downcase
     end
 
-    @current_product == 'front' ? @front_size = size : @divider_size = size
-    puts "Good choice! you selected #{size.upcase}x7ft divider"
+    @product_size = size
+    puts "You selected #{product_size.upcase}x7ft #{current_product.upcase}"
   end
 
   def select_material
-    puts "What material do you want your #{@current_product} to be made of? You can select softwood or plastic."
+    puts "What material do you want your #{current_product.upcase} to be made of? Type SOFTWOOD or PLASTIC"
     material = gets.chomp.downcase
 
     until material == 'softwood' || material == 'plastic' do
@@ -52,8 +72,31 @@ class Configurator
       material = gets.chomp.downcase
     end
 
-    @current_product == 'front' ? @front_material = material : @divider_material = material
-    puts "Good choice! you selected #{material.upcase} material"
+    @product_material = material
+    puts "You selected #{product_material.upcase} material"
+  end
+
+  def select_amount
+    puts "How many of #{current_product.upcase}S do you need?"
+    amount = gets.chomp.to_i
+
+    until amount.is_a?(Integer) do
+      puts "Oops! Looks like typo, please type numbers only!"
+      amount = gets.chomp.to_i
+    end
+
+    @product_amount = amount
+    puts "You selected #{product_amount} #{current_product.upcase}S"
+  end
+
+  def add_product_to_cart
+    class_name = product_range.capitalize + 'Range'
+    object_name = Object.const_get(class_name)
+    object = object_name.new(product_size, product_material, product_amount)
+    p object 
+
+    #add object to hash. key would be number of product added to basket
+    #{1: [size = 8, material = plastic, amount = 2]} if another object variables would math product in the basket just change amount = DOING THAT I CAN'T ADD AMOUNT TO THE OBJECT
   end
 
   # 1st select range
