9a1ea7e2da6d827516a3abd452dfface59f16256
CodeByBlazej <codebyblazej@gmail.com>
2025-05-26 15:02:42 +0100

    add prices

--- lib/configurator/bronze_range.rb
+++ lib/configurator/bronze_range.rb
@@ -1,7 +1,28 @@
 class BronzeRange
+  attr_reader :size, :material, :type, :products, :price
+  attr_accessor :amount
   
-  def initialize size, material
+  def initialize type, size, material, amount
+    @type = type
     @size = size
     @material = material
+    @amount = amount
+    @price = nil
+
+    @products = {
+      'front' => {
+        '8x7ft' => {'softwood' => 493.92, 'plastic' => 544.32},
+        '10x7ft' => {'softwood' => 559.44, 'plastic' => 684.18},
+        '12x7ft' => {'softwood' => 630, 'plastic' => 774.90},
+        '14x7ft' => {'softwood' => 710.64, 'plastic' => 888.30}
+      },
+      'divider' => {
+        '8x7ft' => {'softwood' => 466.20, 'plastic' => 553.77},
+        '10x7ft' => {'softwood' => 539.28, 'plastic' => 665.28},
+        '12x7ft' => {'softwood' => 607.32, 'plastic' => 761.67},
+        '14x7ft' => {'softwood' => 672.84, 'plastic' => 856.17},
+        
+      }
+    }
   end
 end
--- lib/configurator/silver_range.rb
+++ lib/configurator/silver_range.rb
@@ -1,7 +1,28 @@
 class SilverRange
+  attr_reader :size, :material, :type, :products, :price
+  attr_accessor :amount
   
-  def initialize size, material
+  def initialize type, size, material, amount
+    @type = type
     @size = size
     @material = material
+    @amount = amount
+    @price = nil
+
+    @products = {
+      'front' => {
+        '8x7ft' => {'softwood' => 635, 'plastic' => 700},
+        '10x7ft' => {'softwood' => 720, 'plastic' => 881},
+        '12x7ft' => {'softwood' => 772, 'plastic' => 950},
+        '14x7ft' => {'softwood' => 871, 'plastic' => 1089}
+      },
+      'divider' => {
+        '8x7ft' => {'softwood' => 466.20, 'plastic' => 553.77},
+        '10x7ft' => {'softwood' => 539.28, 'plastic' => 665.28},
+        '12x7ft' => {'softwood' => 607.32, 'plastic' => 761.67},
+        '14x7ft' => {'softwood' => 672.84, 'plastic' => 856.17},
+        
+      }
+    }
   end
 end
